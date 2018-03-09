9ba9f63c7f2cf911bc7510d4f28f721480e04c7c
fulang <407628339@qq.com>
2018-03-09 19:28:25 +0800

    Initial Commit

---
+++ NFWebImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NFWebImage
+//
+//  Created by fulang on 2018/3/9.
+//  Copyright © 2018年 fulang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
